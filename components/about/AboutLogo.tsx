2a10a9999d2d14040a0d6654d20acbf1fea7cdab
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-06-28 01:11:44 +0500

    dynamic logo

--- components/about/AboutLogo.tsx
+++ components/about/AboutLogo.tsx
@@ -1,10 +1,11 @@
+import about from "@/settings/about";
 import Image from "next/image";
 
 const AboutLogo = () => {
   return (
     <div className="about-logo w-full">
       <Image
-        src={"/assets/images/about.png"}
+        src={about.logo}
         alt="about logo"
         height={992}
         width={600}
--- settings/about.ts
+++ settings/about.ts
@@ -2,4 +2,5 @@ export default {
   title: "¿Que es Force Academy™?",
   buttonText: "obtén más información",
   buttonHref: "/",
+  logo: "/assets/images/about.png",
 };
