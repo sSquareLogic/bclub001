49f3a923783edb119216e669ef2d61b66c237053
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-07-04 16:13:19 +0500

    about content dynamic

--- settings/about.ts
+++ settings/about.ts
@@ -3,4 +3,9 @@ export default {
   buttonText: "obtén más información",
   buttonHref: "/",
   logo: "/assets/images/about.png",
+  list: [
+    '<span class="uppercase text-white">Force academy™</span> <br/> Es la formación más completa para capacitarte como un barbero de <strong class="text-GOLD">élite</strong>.',
+    'Dónde aprenderás los <strong class="text-GOLD">fundamentos</strong> y las <strong class="text-GOLD">herramientas</strong> enseñadas por barberos <strong class="text-GOLD">premiados</strong>, también la <strong class="text-GOLD">mentalidad</strong> perfecta para emprender tu <strong class="text-GOLD">propio</strong> negocio.',
+    "En esta formacíon también aprenderás el uso de las redes sociales, campañas de marketing, creación y edición de contenido.",
+  ],
 };
