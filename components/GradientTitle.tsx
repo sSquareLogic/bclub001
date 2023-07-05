1732827305ed19f5d881938919a1eafbfdadf78a
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-07-05 07:51:06 +0500

    text align adjustable

--- components/GradientTitle.tsx
+++ components/GradientTitle.tsx
@@ -1,12 +1,13 @@
 interface IProps {
   text: string;
   size?: string;
+  align?: "left" | "right" | "center";
 }
-const GradientTitle = ({ text, size }: IProps) => {
+const GradientTitle = ({ text, size, align = "left" }: IProps) => {
   return (
     <h2
       className="gradient uppercase text-TITLE bg-GRADIENT font-bold"
-      style={size ? { fontSize: size } : {}}
+      style={size ? { fontSize: size, textAlign: align } : { textAlign: align }}
     >
       {text}
     </h2>
