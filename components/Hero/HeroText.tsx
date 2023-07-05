7de6807a4d7c2cdd83aef2e9846f3f24e18ab268
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-07-05 08:01:21 +0500

    hero section corrections

--- components/Hero/HeroText.tsx
+++ components/Hero/HeroText.tsx
@@ -2,16 +2,19 @@ import hero from "@/settings/hero";
 
 const HeroText = () => {
   return (
-    <div className="hero-text flex flex-col gap-10 justify-self-center z-10">
-      <div className="flex flex-col gap-4 ">
-        <h1 className="text-4xl text-TITLE_SM font-bold text-white underline">
+    <div className="hero-text flex flex-col gap-10 justify-self-center justify-center z-10">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-4xl text-TITLE_SM font-bold text-white underline uppercase">
           {hero.team}
         </h1>
-        <h2 className="uppercase text-TITLE_LG font-bold text-white">
+        <h2 className="uppercase text-TITLE_LG font-bold text-white leading-[70px]">
           {hero.title}
         </h2>
       </div>
-      <p className="text-white text-TITLE_SM font-normal">{hero.text}</p>
+      <p
+        className="text-white text-TITLE_SM font-normal leading-10"
+        dangerouslySetInnerHTML={{ __html: hero.text }}
+      ></p>
     </div>
   );
 };
--- settings/hero.ts
+++ settings/hero.ts
@@ -1,6 +1,6 @@
 export default {
   team: "Force Academy™",
   title: "La mejor academia de barbería",
-  text: "“Saber cortar bien, tener tus redes sociales y trabajar duro te llevara al éxito, es un gran mito”",
+  text: "“Saber cortar bien, tener tus redes sociales y trabajar duro te llevara al <strong class='text-white'>éxito</strong>, es un gran <strong class='text-white'>mito”</strong>",
   img: "/assets/images/hero.png",
 };
